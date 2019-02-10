43edd83f665d1a0384e0f438fcf474ee4d76d56d
beamxron <2697297469@qq.com>
2019-02-10 16:02:34 +0800

    修复 bug

---
+++ NOTE.md
@@ -0,0 +1,5 @@
+在桌面版使用vw、vh、vmin、vmax时,缩放无效
+
+内容没有空白水平溢出不会自动换行\
+要添加 `word-break:break-all;`
+
--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,4 @@
 log=console.log
 log("(๑•ᴗ•๑)")
 
-// $(":root").css("font-size",$(window).width()/25)
 
